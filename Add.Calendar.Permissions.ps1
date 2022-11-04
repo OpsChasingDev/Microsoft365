141516610438a90e0cb976a79dbc25a7376b0558
drummermanrob20 <robert@stapleton-family.net>
2022-11-04 09:42:44 -0400

    cleaned up sudo code

--- Add.Calendar.Permissions.ps1
+++ Add.Calendar.Permissions.ps1
@@ -1,26 +1,6 @@
 <#
-function {
-    param (
-        [Paramset 1 and 2]
-        [string]Identity,
-
-        [Paramset 1 and 2]
-        [ValidateSet()]
-        [string]Permission,
-        
-        [Paramset 1]
-        [string[]]User
-        
-        [Paramset 2]
-        [switch]AllUsers,
-    )
-}
-
 - set up connection to 365 exchange using the module ExchangeOnlineManagement and the Connect-ExchangeOnline cmdlet
 
-- identity is a required param
-- either user(s) or all users is a required param (two param sets)
-- permission is a required param (validateset)
 - identity is first checked to make sure it exists
 - if user param is specified, the validity of those accounts is checked as well
 - user is prompted to choose one of the below scenarios based on paramset:
