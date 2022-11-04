0c5d4df52897965384a91bc1cd374f4f7d75f31b
drummermanrob20 <robert@stapleton-family.net>
2022-11-04 08:17:33 -0400

    base outline

--- Add.Calendar.Permissions.ps1
+++ Add.Calendar.Permissions.ps1
@@ -12,10 +12,15 @@ function {
 
 - set up connection to 365 exchange using the module ExchangeOnlineManagement and the Connect-ExchangeOnline cmdlet
 
+- identity is a required param
+- either user(s) or all users is a required param (two param sets)
+- permission is a required param (validateset)
+- identity is first checked to make sure it exists
+- if user param is specified, the validity of those accounts is checked as well
 - user is prompted to choose one of the below scenarios:
-    - Identity's calendar access is being granted to all users
-    - Identity's calendar access is being granted to specified users
-    - All users' calendar access is being granted to Identity
-    - Specified users' calendar access is being granted to Identity
+    - (1) Identity's calendar access is being granted to all users
+    - (2) Identity's calendar access is being granted to specified users
+    - (3) All users' calendar access is being granted to Identity
+    - (4) Specified users' calendar access is being granted to Identity
 #>
 
