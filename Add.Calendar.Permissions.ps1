9113ae60b281b5ce524c5ed274fbc45479e2bd12
drummermanrob20 <robert@stapleton-family.net>
2022-11-28 09:19:54 -0500

    synopsis

--- Add.Calendar.Permissions.ps1
+++ Add.Calendar.Permissions.ps1
@@ -7,6 +7,21 @@ Next Steps:
 #>
 
 function Add-M365CalendarPermission {
+    <#
+    .SYNOPSIS
+        Adds permissions to Microsoft 365 mailbox calendars.
+    .DESCRIPTION
+        A longer description of the function, its purpose, common use cases, etc.
+    .NOTES
+        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .LINK
+        Specify a URI to a help page, this will show when Get-Help -Online is used.
+    .EXAMPLE
+        Test-MyTestFunction -Verbose
+        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    #>
+    
+    
     [Cmdletbinding(DefaultParameterSetName = "default",
         SupportsShouldProcess,
         ConfirmImpact = "Medium")]
